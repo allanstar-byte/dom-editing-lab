2db1e6f14d7d3cae5b755d9f8f8e416a3dea9e64
Allan Ngoma <angoma@zamara.co.ke>
2022-06-28 08:58:18 +0300

    dom

---
+++ index.js
@@ -0,0 +1,7 @@
+// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Stanley Mayore is the champion";
